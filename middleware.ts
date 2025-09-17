8a68d7e0ef2a4d413d2acc07c6d47bcbbddc1273
poolr64py-pixel <poolr64py@gmail.com>
2025-09-17 14:15:11 -0300

    Transforma imobiliario-frontend em pasta normal, sem submódulo

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './src/lib/routing';
-
-export default createMiddleware(routing);
-
-export const config = {
-  // Corresponder apenas aos pathnames internacionalizados
-  matcher: ['/', '/(pt-BR|es-PY|en-US|gn-PY)/:path*']
-};
--- src/components/sections/AboutSection.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function AboutSection() {
-  return <div>About Section</div>;
-}
